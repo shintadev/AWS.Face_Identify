0f53ada15762fa092361e9615c6fabf583df5462
Nikin-2204 <truonglnh.kv@gmail.com>
2023-11-13 08:33:07 +0900

    update

--- lambda/search_face_in_images/app.py
+++ lambda/search_face_in_images/app.py
@@ -1,4 +1,3 @@
-
 import boto3
 import os
 import json
@@ -6,12 +5,10 @@ import json
 #Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
-collection_id = os.environ['COLLECTION_ID']
-table_name = os.environ['table_name']
-s3 = boto3.client('s3')
+collection_id = os.environ('COLLECTION_ID')
+table_name = os.environ('table_name')
 dynamodb = boto3.client('dynamodb')
 client=boto3.client('rekognition', region_name = 'ap-northeast-1')
-
 
 def lambda_handler(event, context):
     #get URI from UI
@@ -29,19 +26,14 @@ def lambda_handler(event, context):
             'Content-Type': 'application/json'
         },
         'body': json.dumps(listObj)
-    }
-    
+    }    
     return response
-            
-            
-
-        
-            
+   
 
 def rekogintion_search_faces(uri):
     uri = uri.split("/")
     bucket= uri[2]
-    collectionId='collection_id'
+    collectionId = collection_id
     fileName= uri[3] + "/" +uri[4]
     threshold = 70
     maxFaces=5
@@ -52,11 +44,9 @@ def rekogintion_search_faces(uri):
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         return response['FaceMatches']['Face']['FaceId']
         
-
 def dynamoDB_search(facesId):   
     # Define the list of values
     partition_key_values = facesId
-
     query_params = {
         'TableName': table_name,
         'KeyConditionExpression': 'FaceId IN (:pk_values)',
@@ -64,7 +54,6 @@ def dynamoDB_search(facesId):
         ':pk_values': {'SS': partition_key_values}
         }
     }
-      
     response = dynamodb.query(**query_params) 
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         return response['Items']                         
