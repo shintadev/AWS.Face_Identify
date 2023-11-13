d59f4004f6fc340a85260f1bdacd6579a4c4c5ca
Nikin-2204 <truonglnh.kv@gmail.com>
2023-11-13 10:13:12 +0900

    update dynamo part in searchlambda

--- lambda/search_face_in_images/app.py
+++ lambda/search_face_in_images/app.py
@@ -5,8 +5,8 @@ import json
 #Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
-collection_id = os.environ('COLLECTION_ID')
-table_name = os.environ('table_name')
+collection_id = os.environ['COLLECTION_ID']
+table_name = os.environ['table_name']
 dynamodb = boto3.client('dynamodb')
 client=boto3.client('rekognition', region_name = 'ap-northeast-1')
 
@@ -15,17 +15,22 @@ def lambda_handler(event, context):
     uri = event['uri']
     
     #Search face in collection
-    facesID = rekogintion_search_faces(uri)
+    faces = rekogintion_search_faces(uri)
     
     #Get OBJ from DB
-    listObj = dynamoDB_search(facesID)
+    
+    db = dynamoDB_search(faces)
 
+    
+    #Create Obj to response
+    obj = obj_responese(db, faces)
+    
     response = {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body': json.dumps(listObj)
+        'body': json.dumps(obj)
     }    
     return response
    
@@ -42,22 +47,46 @@ def rekogintion_search_faces(uri):
                                 FaceMatchThreshold=threshold,
                                 MaxFaces=maxFaces)
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return response['FaceMatches']['Face']['FaceId']
+        return response['FaceMatches']['Face']
         
-def dynamoDB_search(facesId):   
+def dynamoDB_search(faces):   
     # Define the list of values
+    facesId = []
+    for i in faces:
+        facesId.append(i.FaceId)
     partition_key_values = facesId
     query_params = {
         'TableName': table_name,
         'KeyConditionExpression': 'FaceId IN (:pk_values)',
         'ExpressionAttributeValues': {
-        ':pk_values': {'SS': partition_key_values}
+        ':pk_values': {'S': partition_key_values}
         }
     }
     response = dynamodb.query(**query_params) 
     if response['ResponseMetadata']['HTTPStatusCode'] == 200:
         return response['Items']                         
         
+class obj_to_responese:
+  def __init__(self, faceId, boundingbox, name, uri):
+        self.faceId = faceId 
+        self.boundingbox = boundingbox 
+        self.name = name 
+        self.uri = uri 
+
+def obj_responese(db, re):
+    list_response = []
+    for i in db:
+        for y in re:
+            if i.FaceID == y.FaceId:
+                list_response.append(
+                    obj_to_responese(
+                        i['FaceID'],y['BoundingBox'],i['Name'],i['BucketLocation']
+                    )
+                ) 
+                break
+    return list_response            
 
 
-            
+
+
+  
